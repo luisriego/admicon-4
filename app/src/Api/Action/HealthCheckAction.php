0d98202539cb819292fb4a55bcf5ba503f758457
luisriego <luisriego@hotmail.com>
2021-09-16 21:05:56 -0300

    Listener configution Ok

--- app/src/Api/Action/HealthCheckAction.php
+++ app/src/Api/Action/HealthCheckAction.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Api\Action;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
